3ddfe607b707fac1020b718be6baf95e9389acd8
Gerhard Maree <Gerhardus.Maree@sms-digital.com>
2021-11-29 20:54:56 +0100

    Add environment monitor values

---
+++ EnviroMonitor/EnviroMonitor/Migrations/20211129195243_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EnviroMonitor.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EnvironmentReadings",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    RZero = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CorrectedRZero = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Resistance = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PPM = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Temperature = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Humidity = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EnvironmentReadings", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EnvironmentReadings");
+        }
+    }
+}
---
+++ EnviroMonitor/EnviroMonitor/Models/Weather.cs
@@ -0,0 +1,9 @@
+﻿namespace EnviroMonitor.Models
+{
+    public class Weather
+    {
+        public string Temperature { get; set; }
+        public string Humidity { get; set; }
+        public string HeatIndex { get; set; }
+    }
+}
